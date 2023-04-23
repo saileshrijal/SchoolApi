6f7705e123ce25435be36872fd5b621537f3b1f8
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-04-23 11:37:56 +0545

    database connection

---
+++ SchoolApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolApi.Models;
+
+namespace SchoolApi.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+                        
+        }
+        public DbSet<Parent>? Parents { get; set; }
+    }
+}
---
+++ SchoolApi/Models/Parent.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolApi.Models
+{
+    public class Parent
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+    }
+}
